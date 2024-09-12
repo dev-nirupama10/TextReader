bd0bcc97933719b1cb1076945b52e957f3ace9f5
Nirupama <nirupamasoundarr@gmail.com>
2024-09-12 13:12:14 +0530

    Task completed

---
+++ js/script.js
@@ -0,0 +1,115 @@
+// Selecting the text area
+const textArea = document.getElementById("textArea");
+
+// Selecting the submit button
+const submitBtn = document.getElementById("btn");
+
+// Selecting the reset button
+const resetBtn = document.getElementById("reset");
+
+// Selecting result div
+const resultDiv = document.querySelector(".result");
+
+// Selecting result text paragraphs
+const characterCountText = document.getElementById("charCount");
+const wordCountText = document.getElementById("wordCount");
+const uniqueWordCountText = document.getElementById("uniqueWordCount");
+const countByUniqueWord = document.getElementById("countByUniqueWord");
+
+// Adding event listener for the reset button
+resetBtn.addEventListener("click", () => {
+  // Clearing the text area
+  textArea.value = "";
+  resultDiv.classList.add("hidden");
+  console.log(resultDiv.classList);
+  characterCountText.textContent = "";
+  wordCountText.textContent = "";
+  uniqueWordCountText.textContent = "";
+  countByUniqueWord.innerHTML = "";
+});
+
+// Adding event listener to the submit button to handle a click event
+submitBtn.addEventListener("click", () => {
+  // Extracting the text area contents
+  const text = textArea.value;
+
+  // Empty text validation
+  if (text.length === 0) {
+    alert("Please enter a text to read!");
+    return;
+  }
+
+  // Finding words count in the text
+  const wordsCount = text.split(" ").length;
+
+  // Seperating characters in the text area
+  const characters = text.split("");
+
+  // Counting the number of alphabet characters in the text area contents using Reduce()
+  const characterCount = characters.reduce((acc, character) => {
+    if (isAlphabet(character)) {
+      acc++;
+    }
+    return acc;
+  }, 0);
+
+  // Finding unique words and their counts
+  const wordsSet = new Set(text.split(" "));
+  const uniqueWordCount = wordsSet.size;
+
+  const uniqueWords = {};
+  const cleanText = removePunctuation(text);
+
+  const words = cleanText.split(" ");
+  words.forEach((word) => {
+    if (uniqueWords[word]) {
+      uniqueWords[word]++;
+    } else {
+      uniqueWords[word] = 1;
+    }
+  });
+
+  // Appending result to the DOM elements
+  characterCountText.textContent = `Character Count: ${characterCount}`;
+  wordCountText.textContent = `Word Count: ${wordsCount}`;
+  uniqueWordCountText.textContent = `Unique Word Count: ${uniqueWordCount}`;
+
+  const title = document.createElement("h3");
+
+  title.classList.add("title");
+
+  title.innerHTML = `Count By Unique Word`;
+
+  countByUniqueWord.appendChild(title);
+
+  const containerDiv = document.createElement("div");
+  containerDiv.classList.add("unique-word-div"); //white box
+
+  for (const word in uniqueWords) {
+    const div = document.createElement("div");
+    const p = document.createElement("p");
+    p.innerHTML = `${word}: ${uniqueWords[word]}`;
+    div.appendChild(p);
+    containerDiv.appendChild(div);
+  }
+  countByUniqueWord.appendChild(containerDiv);
+
+  textArea.value = "";
+  resultDiv.classList.remove("hidden");
+});
+
+function isAlphabet(character) {
+  const alphabets = "abcdefghijklmnopqrstuvwxyz";
+
+  return alphabets.includes(character.toLowerCase());
+}
+
+function removePunctuation(text) {
+  let cleanText = "";
+  for (let i = 0; i < text.length; i++) {
+    if (isAlphabet(text[i]) || text[i] === " ") {
+      cleanText += text[i];
+    }
+  }
+  return cleanText;
+}
---
+++ readme.md
@@ -0,0 +1,31 @@
+# Simple Text Reader
+
+This is a simple Text Reader web application built using HTML, CSS, and JavaScript. The application takes input from a text area and provides real-time statistics such as the total number of characters, words, and unique words. It also includes a reset button to clear the input and reset the statistics.
+
+## Features
+
+- **Character Count**: Displays the total number of characters entered in the text area.
+- **Word Count**: Displays the total number of words in the text.
+- **Unique Word Count**: Displays the number of unique words in the text.
+- **Unique Words**: Displays the unique words in the text.
+- **Reset Button**: Resets the text area input and clears the displayed statistics.
+- **Responsive UI**: The CSS is completely responsive making it user friendly across all devices.
+
+## Demo
+
+![Text Reader Screenshot](./Demo%20screenshot.png)
+
+## How It Works
+
+1. Enter text into the provided text area.
+2. Press the "Submit" button to see:
+   - Total number of characters.
+   - Total number of words.
+   - Total number of unique words.
+3. Press the "Reset" button to clear the input and reset the statistics.
+
+## Technologies Used
+
+- **HTML**: Structure of the web page.
+- **CSS**: Styling for the layout and design of the application.
+- **JavaScript**: Handles the logic for counting characters, words, and unique words, and updating the DOM.
